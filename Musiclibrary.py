a0cb8a73306a960d5531b0a75e08dabfa7eeadc8
Sumbul457 <sumbulsaleem907@gmail.com>
2026-07-28 13:33:47 +0500

    Delete Musiclibrary.py

--- Musiclibrary.py
+++
@@ -1,8 +0,0 @@
-
-music = {
-    "why" : "https://www.youtube.com/watch?v=pm8TTOd4b88&pp=ygUEMTk4NQ%3D%3D",
-     "fine" : "https://www.youtube.com/watch?v=OpKEdxbW5wg&list=RDOpKEdxbW5wg&index=1&pp=8AUB",
-     "okay" : "https://www.youtube.com/watch?v=xAR6N9N8e6U",
-     "sky" : "https://www.youtube.com/watch?v=ATSqA11jVBU&list=RDOpKEdxbW5wg&index=3&pp=8AUB",
-     "indila" : "https://www.youtube.com/watch?v=sL8-vrfTR7k&list=RDOpKEdxbW5wg&index=4&pp=8AUB"
-}
